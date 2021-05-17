94ab2cd322884481235a3f5a4950915dee5da6bf
Eduardo Tongson <propolice@gmail.com>
2021-05-17 12:35:53 +0800

    Better runners.

--- runner/backend.lua
+++ runner/backend.lua
@@ -1,6 +1,8 @@
-local podman = require("podman")
-podman.config({
-	NAME = "mongo",
+podman = require("podman")
+idmap = podman.reserve_idmap()
+mongo = podman.config({
+	NAME = "db",
+	BASE = "mongo",
 	URL = "docker://docker.io/library/mongo",
 	TAG = "4.4",
 	NETWORK = "private",
@@ -9,26 +11,27 @@ podman.config({
 	MEM = "512m",
 	ENVIRONMENT = { MONGO_INITDB_ROOT_USERNAME = "root", MONGO_INITDB_ROOT_PASSWORD = "1234" },
 })
-podman.start("mongo")
-podman.config({
+mongo:start()
+backend = podman.config({
 	NAME = "backend",
 	URL = "filestage-todo-backend",
 	TAG = "test",
-	NETWORK = "mongo",
+	NETWORK = "db",
 	CPUS = "3",
 	SHARES = "512",
+	IDMAP = idmap,
 	MEM = "256m",
 })
-podman.start("backend", 600)
---[[podman.config({
+backend:start(600)
+fuzz = podman.config({
 	NAME = "backend",
 	URL = "filestage-todo-backend",
 	TAG = "fuzz",
-	NETWORK = "mongo",
+	NETWORK = "db",
 	CPUS = "3",
 	SHARES = "512",
 	MEM = "256m",
 })
-podman.start("backend", 1000)
-]]
-podman.stop("mongo")
+fuzz:start(1000)
+mongo:stop()
+podman.release_idmap(idmap)
--- runner/frontend.lua
+++ runner/frontend.lua
@@ -1,28 +1,5 @@
-local podman = require("podman")
---[[
-podman.config({
-	NAME = "mongo",
-	URL = "docker://docker.io/library/mongo",
-	TAG = "4.4",
-	NETWORK = "private",
-	CPUS = "2",
-	SHARES = "512",
-	MEM = "512m",
-	ENVIRONMENT = { MONGO_INITDB_ROOT_USERNAME = "root", MONGO_INITDB_ROOT_PASSWORD = "1234" },
-})
-podman.start("mongo")
-podman.config({
-	NAME = "backend",
-	URL = "filestage-todo-backend",
-	TAG = "final",
-	NETWORK = "mongo",
-	CPUS = "3",
-	SHARES = "512",
-	MEM = "256m",
-})
-podman.start("backend")
-]]
-podman.config({
+podman = require("podman")
+frontend = podman.config({
 	NAME = "frontend",
 	URL = "filestage-todo-frontend",
 	TAG = "test",
@@ -31,4 +8,4 @@ podman.config({
 	SHARES = "512",
 	MEM = "512m",
 })
-podman.start("frontend", 600)
+frontend:start(600)
